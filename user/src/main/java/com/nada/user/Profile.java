3a5456eac017c2bdb680a6d2901d924e3e61d230
Troy Hart <troy.hart@gmail.com>
2014-03-15 00:23:53 -0600

    Removed application of versioning.gradle from from root project, plus a little cleanup.
    By removing the application of the versioning script from the root
    project, all sub-projects are empowered to dynamically manage discrete
    versioning information. When versioning.gradle is applied in the
    "allprojects" configuration sub-projects don't pick up the timestamp
    property. However, when applied in the "subprojects" configuration
    each sub-project manages a discrete timestamp file in the build
    directory.
    
    I formatted the source code of the versioning.gradle file and that
    caused a lot of noise. There was only a tiny bit of actual content
    change in this file--see the assignment of ext.rcNumber.

--- user/src/main/java/com/nada/user/Profile.java
+++ user/src/main/java/com/nada/user/Profile.java
@@ -1,10 +1,9 @@
 package com.nada.user;
+
 /* Copyright (C) 2014 Partnet, Inc.  Confidential and Proprietary. */
 
 /**
  * @author thart
- * it's modified.
- * a change
  */
 public class Profile
 {
